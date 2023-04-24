31bd3b337ebb9f2db0533f235198491fa4f5eb09
PedrodoDivino <pedrohenriquebatera@hotmail.com>
2023-04-24 00:16:17 -0300

    [Pedro]  ajustes

--- docs/utils/average.js
+++ docs/utils/average.js
@@ -3,19 +3,20 @@ function media() {
     const mediaTwo = Number(document.getElementById('mediaTwo').value);
     const mediaThree = Number(document.getElementById('mediaTheree').value);
     const mediaFour = Number(document.getElementById('mediaFour').value);
+    const name = document.getElementById('name').value;
     let result = (mediaOne + mediaTwo + mediaThree + mediaFour) / 4;
 
     document.getElementById('result').innerHTML = result  
     
     if (result >= 6) { 
-        document.getElementById('result').innerHTML = result.innerHTML = "aprovado"  
+        document.getElementById('result').innerHTML = result.innerHTML = `o aluno ${name} foi aprovado`;
         document.getElementById('result').style.backgroundColor = "green";
     }
     if (result >2 && result <6) { 
-        document.getElementById('result').innerHTML = result.innerHTML = "recuperação"  
+        document.getElementById('result').innerHTML = result.innerHTML = `o aluno ${name} ficou recuperação`  
          document.getElementById('result').style.backgroundColor = "blue";
     } if (result <=2) { 
-        document.getElementById('result').innerHTML = result.innerHTML = "Paranbéns REPROVADO"  
+        document.getElementById('result').innerHTML = result.innerHTML = `Paranbéns o aluno ${name} foi REPROVADO`  
          document.getElementById('result').style.backgroundColor = "red";
     }
 }
--- docs/utils/form.js
+++ docs/utils/form.js
@@ -1,13 +1,20 @@
-function getData() {
+function getData(tech) {
     const name = document.getElementById('nameUser').value;
     const lastName = document.getElementById('lastName').value;
     const city = document.getElementById('city').value;
     const state = document.getElementById('civilStates').value;
     const instagram = document.getElementById('instagram').value;
-    const gender = document.getElementById('gender').value;
-    const resut = `Olá  ${name} ${lastName}  <br>
-        sua cidade é  ${city} <br> seu estado civil é ${state}<br>  seu instagram é @ ${instagram}<br> 
-        e voce  é  ${gender} <br><br> obrigado por visitar o meu cadasdtro XD`;
-    document.getElementById('result').innerHTML = resut
-}
+    const result =  document.getElementById('result');
 
+    if (document.getElementById('dart').checked) {
+        tech = document.getElementById('dart').value;
+     }
+
+    if (document.getElementById('react').checked) {
+         tech = document.getElementById('react').value;
+     }
+
+   result.innerHTML = `Olá  ${name} ${lastName}  <br>
+   sua cidade é  ${city} <br> seu estado civil é ${state}<br>  seu instagram é @ ${instagram}<br> 
+e você tem interesse em ${tech}<br><br> obrigado por visitar o meu cadasdtro XD`;
+}
